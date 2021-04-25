75c46f74250b09309bb31dfdd022ad1d531d3ca5
LS <ls@MacBook-Air-de-PG.local>
2021-04-25 16:45:27 -0300

    fix: add drawcontours function

--- computer_vision.py
+++ computer_vision.py
@@ -5,23 +5,29 @@ from playsound import playsound
 
 class ComputerVisionCapture:
     has_finger_on_the_screen = False
-    ratio = 2.0
 
-    def detect_shape(self, contour):
-        discovered_shape = ""
+    def __init__(self, ratio=2.0):
+        self.ratio = ratio
+
+    def draw_contours(contours, text=None, img_to_draw, contour_color=(0,0,255), color_thickness=1):
+        color_thickness = 1
+        cv2.drawContours(img_to_draw, contours, -1, contour_color, color_thickness)
+        if text:
+            pass
+            # cv2.putText(frame, shape, (cX, cY), 0.5, (255, 255, 255), 2)
+
+    def is_rectangle(self, contour):
         peri = cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, 0.04 * peri, True)
-        if len(approx) == 4:
-            return "rectangle"
-        return "unknown shape"
+        return True if len(approx) == 4 else False
 
     def rescale_frame(self, frame, scale=0.5):
         """
             Resize the frame is good to have an improvement
             of the capability to read the image
         """
-        frame_width = frame.shape[1]
         frame_height = frame.shape[0]
+        frame_width = frame.shape[1]
         dimensions = int(frame_width*scale), int(frame_height*scale)
 
         return cv2.resize(frame, dimensions)
@@ -37,9 +43,17 @@ class ComputerVisionCapture:
 
     def find_countours(self, img):
         # cv2.RETR_EXTERNAL
-        contours, hierarchies = cv2.findContours(img.copy(), cv2.RETR_LIST,
-                                                 cv2.CHAIN_APPROX_NONE)
-        return contours, hierarchies
+        # TODO: test cv2.RETR_TREE instead RETR_LIST
+        # TODO: test cv.CHAIN_APPROX_SIMPLE instead CHAIN_APPROX_NONE
+        all_contours_found = cv2.findContours(img.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+        # contours_with_good_area_size = []
+        # remove small and large contours (small countor area certainly won't be a key)
+        # for contour in all_contours_found:
+        #     area = cv2.contourArea(contour, True)
+        #     if 100 < area < 2000:
+        #         contours_with_good_area_size.append(contour)
+
+        return all_contours_found
 
     def read_camera(self):
         cv2_webcam_number = 0
@@ -51,32 +65,18 @@ class ComputerVisionCapture:
 
             # threshold makes above 225 color will be black, under 125 will be white
             # test thresh vs canny
-            thresh = cv2.threshold(
-                cleaned_image, 125, 255, cv2.THRESH_BINARY_INV)[1]
+            thresh = cv2.threshold(cleaned_image, 125, 255, cv2.THRESH_BINARY_INV)[1]
+            
+            # https://docs.opencv.org/master/dd/d49/tutorial_py_contour_features.html
+            contours = self.find_countours(thresh)
 
-            # cv2.imshow('blurred_img', self.clean_image_noises(frame))
+            self.draw_contours(cleaned_image, contours)
+            self.draw_contours(rescaled_image, contours)
+                
+
             cv2.imshow("Blurred image", cleaned_image)
             cv2.imshow("Thresholded image", thresh)
-            contours = self.find_countours(thresh)[0]
-
-            new_cnts = [c for c in contours if 300 < cv2.contourArea(c) < 4000]
-            new_cnts = [c for c in new_cnts if cv2.contourArea(c, True) > 0]
-            for c in range(len(new_cnts)):
-                # compute the center of the contour
-                M = cv2.moments(new_cnts[c])
-                # if going to divide by 0 then skip
-                if M["m00"] != 0:
-                    cX = int(M["m10"] / M["m00"] * self.ratio)
-                    cY = int(M["m01"] / M["m00"] * self.ratio)
-
-                shape = self.detect_shape(new_cnts[c])
-
-                new_cnts[c] = new_cnts[c].astype("float")
-                new_cnts[c] *= self.ratio
-                new_cnts[c] = new_cnts[c].astype("int")
-                cv2.drawContours(frame, [new_cnts[c]], -1, (0, 255, 0), 2)
-                cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.5, (255, 255, 255), 2)
+            cv2.imshow("Webcam normal rescaled image", rescaled_image)
 
             # press ESC to exit
             if cv2.waitKey(1) == 27:
