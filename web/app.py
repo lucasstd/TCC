881e9bcae763106d4e6bc212f44e797bda993197
lucasstd <lucas.staudt@acad.pucrs.br>
2021-06-11 23:13:58 -0300

    feat: pass url to index request

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 import json
 import cv2
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, abort
 
 from computer_vision import computer_vision
 
@@ -14,27 +14,10 @@ with open("songs.json", "r") as songs_file:
 app = Flask(__name__)
 
 
-# https://stackoverflow.com/questions/57762334/image-streaming-with-opencv-and-flask-why-imencode-is-needed
-def get_contours_in_image():
-    camera = cv2.VideoCapture(0)
-    fingertips_indexes = [10, 10, 10, 10, 10]
-    while True:
-        processed_image, fingertips_indexes = computer_vision.process_image(
-            fingertips_indexes, camera
-        )
-
-        _, buffer = cv2.imencode('.jpg', processed_image)
-        buffered_image = buffer.tobytes()
-        yield (
-            b"--frame\r\n"
-            b"Content-Type: image/jpeg\r\n\r\n%s\r\n" % buffered_image
-        )
-
-
 @app.route('/get-contours')
 def get_contours_image():
     return Response(
-        get_contours_in_image(),
+        computer_vision.get_contours_in_image(),
         mimetype='multipart/x-mixed-replace; boundary=frame'
     )
 
@@ -46,7 +29,12 @@ def index():
 
 @app.route("/song/<song_id>")
 def song_tutorial(song_id):
-    return render_template('song.html', music_link="https://www.youtube.com/watch?v=7eQBm-j8Ev0")
+    song = songs.get(song_id)
+    for song_difficulty in songs:
+        song_details = songs[song_difficulty].get(song_id)
+        if song_details:
+            return render_template('song.html', song=song_details)
+    abort(404)
 
 
 if __name__ == "__main__":
