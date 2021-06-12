c49853925f26748b48684c4f876c2abc90adc323
lucasstd <lucas.staudt@acad.pucrs.br>
2021-06-12 15:10:37 -0300

    feat: ADD music and style to index.html

--- WIP/play_sounds.py
+++
@@ -1,8 +0,0 @@
-# from _thread import start_new_thread
-import simpleaudio as sa
-
-
-def play_note_by_key_place(piano_key: int) -> None:
-    notes = { 1: "C", 2: "D", 3: "E", 4: "F", 5: "G", 6: "A", 7: "B" }
-    wave_obj = sa.WaveObject.from_wave_file(f"sounds/{notes.get(piano_key, 'C')}.wav")
-    play_obj = wave_obj.play()
